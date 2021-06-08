3845c9cddb84ecdb855f33d1cc60ca0ce78a645e
Juel07 <juel.braganza97@gmail.com>
2021-06-08 11:59:04 +0100

    add context to tests

--- secret-diary/spec/secretdiary_spec.rb
+++ secret-diary/spec/secretdiary_spec.rb
@@ -1,20 +1,11 @@
 require "secret_diary"
 
 describe SecretDiary do
-  #   it "can add entry" do
-  #     expect(subject.add_entry("My name is Jen")).to eq(["My name is Jen"])
-  #   end
-  #   it "can get all entries" do
-  #     subject.add_entry("note 1")
-  #     subject.add_entry("note 2")
-  #     expect(subject.get_entries).to eq(["note 1", "note 2"])
-  #   end
-  #   it "can be locked" do
-  #     expect(subject.lock).to eq(true)
-  #   end
-  #   it "can be unlocked" do
-  #     expect(subject.unlock).to eq(false)
-  #   end
+  it "can be locked" do
+    subject.unlock
+    expect(subject.lock).to eq(true)
+  end
+
   context "When diary is locked" do
     it "throws error when adding entry" do
       subject.lock
@@ -23,6 +14,24 @@ describe SecretDiary do
     it "throws error when getting entries" do
       subject.lock
       expect { subject.get_entries }.to raise_error("Diary locked!")
+    end
+  end
+
+  it "can be unlocked" do
+    subject.unlock
+    expect(subject.unlock).to eq(false)
+  end
+
+  context "when diary is unlocked" do
+    it "can add entry" do
+      subject.unlock
+      expect(subject.add_entry("My name is Jen")).to eq(["My name is Jen"])
+    end
+    it "can get all entries" do
+      subject.unlock
+      subject.add_entry("note 1")
+      subject.add_entry("note 2")
+      expect(subject.get_entries).to eq(["note 1", "note 2"])
     end
   end
 end

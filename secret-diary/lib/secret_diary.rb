105ba2a6a3dea6e707c6ac7be8abbfb516322c1d
Juel07 <juel.braganza97@gmail.com>
2021-06-08 11:25:48 +0100

    can get all entries + lock diary

--- secret-diary/lib/secret_diary.rb
+++ secret-diary/lib/secret_diary.rb
@@ -1,9 +1,18 @@
 class SecretDiary
   def initialize
     @notes = []
+    @locked = false
   end
 
   def add_entry(note)
     @notes << note
+  end
+
+  def get_entries
+    @notes
+  end
+
+  def lock
+    @locked = true
   end
 end
--- secret-diary/spec/secretdiary_spec.rb
+++ secret-diary/spec/secretdiary_spec.rb
@@ -4,4 +4,12 @@ describe SecretDiary do
   it "can add entry" do
     expect(subject.add_entry("My name is Jen")).to eq(["My name is Jen"])
   end
+  it "can get all entries" do
+    subject.add_entry("note 1")
+    subject.add_entry("note 2")
+    expect(subject.get_entries).to eq(["note 1", "note 2"])
+  end
+  it "can be locked" do
+    expect(subject.lock).to eq(true)
+  end
 end

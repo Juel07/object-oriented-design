b7d4b51ce64225d8141e66551d2be3f78bd3dd60
Juel07 <juel.braganza97@gmail.com>
2021-06-10 15:14:19 +0100

    upload exercise

---
+++ doubles-exercise/lib/airport.rb
@@ -0,0 +1,34 @@
+require "weather"
+
+class Airport
+  attr_reader :planes
+
+  def initialize(planes = [], weather = Weather.new)
+    @planes = planes
+    @weather = weather
+  end
+
+  def land(plane)
+    check_weather("land")
+    @planes.push(plane)
+  end
+
+  def take_off
+    check_weather("take off")
+    @planes.pop
+    @planes
+  end
+
+  def plane_at_airport?(plane)
+    @planes.include?(plane)
+  end
+
+  def check_weather(action)
+    stormy = 2
+    if @weather.generate == stormy
+      raise "cannot #{action} due to stormy weather"
+    else
+      "perfect for flying"
+    end
+  end
+end
